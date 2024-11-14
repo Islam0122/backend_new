7b46006ac9ba309d5a0707942e48d25082891097
islam <dujsobaevislam01@gmail.com>
2024-11-14 23:54:17 +0600

    8 commit

--- backend/core/settings/cors.py
+++ backend/core/settings/cors.py
@@ -22,5 +22,9 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-# Разрешаем доступ для всех доменов
+# CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS', cast=csv())
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:*",  # Разрешаем доступ с любых портов на localhost
+    "https://duishobaevislam01.up.railway.app",  # Разрешаем доступ с продакшн сервера
+]
