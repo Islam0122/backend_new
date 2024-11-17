244e50c2588aea930bc3dff68bed2582f6b94c49
islam <dujsobaevislam01@gmail.com>
2024-11-17 22:48:48 +0600

    10 commit

--- backend/core/settings/cors.py
+++ backend/core/settings/cors.py
@@ -21,20 +21,20 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
     'x-requested-with',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ORIGIN_ALLOW_ALL = True # Отключаем разрешение всех доменов
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500/index.html",
-     "http://127.0.0.1:5500",  # порт Go Live
-    "http://localhost:5500",  # если Go Live работает на localhost
-    "http://localhost:63343",  # Your local frontend
-        "https://duishobaevislam01.up.railway.app",  # ваш сервер на Railway
-]
+# CORS_ALLOW_ALL_ORIGINS = True
+#
+# # CORS_ORIGIN_ALLOW_ALL = True # Отключаем разрешение всех доменов
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500/index.html",
+#      "http://127.0.0.1:5500",  # порт Go Live
+#     "http://localhost:5500",  # если Go Live работает на localhost
+#     "http://localhost:63343",  # Your local frontend
+#         "https://duishobaevislam01.up.railway.app",  # ваш сервер на Railway
+# ]
 CORS_ORIGIN_WHITELIST = [
-    "http://127.0.0.1:5500/index.html",
+    # "http://127.0.0.1:5500/index.html",
     "http://127.0.0.1:5500",  # порт Go Live
     "http://localhost:5500",  # если Go Live работает на localhost
     "http://localhost:63343",  # Your local frontend
-        "https://duishobaevislam01.up.railway.app",  # ваш сервер на Railway
+    "https://duishobaevislam01.up.railway.app",  # ваш сервер на Railway
 ]
