35777cecfe196dce4cfe766cff7a08325538eba9
islam <dujsobaevislam01@gmail.com>
2024-11-18 14:29:41 +0600

    15 commit

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -132,6 +132,13 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # Разрешает доступ всем, включая анонимных пользователей
+    ],
 }
 LANGUAGE_CODE = 'en-us'
 
