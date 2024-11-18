a38c7db3eb6cbe94311052b6cda9b407affe89b1
islam <dujsobaevislam01@gmail.com>
2024-11-18 15:29:38 +0600

    22 commit

--- backend/app/chat_ai/serializers.py
+++ backend/app/chat_ai/serializers.py
@@ -11,23 +11,23 @@ class TalesSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-    def create(self, validated_data):
-        try:
-            text = validated_data.get('text')
-            generated_story = sent_prompt_and_get_response(text)
-            validated_data['text'] = generated_story
-
-            user = self.context['request'].user
-
-            # Для анонимных пользователей сохраняем сказку без привязки к пользователю
-            if user.is_anonymous:
-                validated_data['user'] = None  # Не привязываем пользователя, если он аноним
-            else:
-                validated_data['user'] = user  # Если пользователь авторизован, сохраняем его
-
-            return super().create(validated_data)
-        except Exception as e:
-            raise serializers.ValidationError({"detail": f"Ошибка при создании сказки: {str(e)}"})
+    # def create(self, validated_data):
+    #     try:
+    #         text = validated_data.get('text')
+    #         generated_story = sent_prompt_and_get_response(text)
+    #         validated_data['text'] = generated_story
+    #
+    #         user = self.context['request'].user
+    #
+    #         # Для анонимных пользователей сохраняем сказку без привязки к пользователю
+    #         if user.is_anonymous:
+    #             validated_data['user'] = None  # Не привязываем пользователя, если он аноним
+    #         else:
+    #             validated_data['user'] = user  # Если пользователь авторизован, сохраняем его
+    #
+    #         return super().create(validated_data)
+    #     except Exception as e:
+    #         raise serializers.ValidationError({"detail": f"Ошибка при создании сказки: {str(e)}"})
 
 
 
