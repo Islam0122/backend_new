14e174c52ac1f762908aeef359c47dc48dc46e25
islam <134096989+Islam0122@users.noreply.github.com>
2024-11-20 19:27:12 +0600

    Update tale_ai_function.py

--- backend/app/chat_ai/tale_ai_function.py
+++ backend/app/chat_ai/tale_ai_function.py
@@ -2,31 +2,25 @@ import base64
 import json
 import os
 import uuid
-from typing import Any
-
 import requests
 from requests.auth import HTTPBasicAuth
-
 CLIENT_ID='71b92890-bf91-4b6b-9645-6561b93e3d7d'
 SECRET='3278c7e4-6c0c-4b7b-a8b7-9baadb679504'
-
-
-def get_access_token() -> Any | None:
+def get_access_token() -> str:
     url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'application/json',
-        'RqUID': str(uuid.uuid4()),  # уникальный идентификатор запроса
+        'RqUID': str(uuid.uuid4()),
     }
     payload = {"scope": "GIGACHAT_API_PERS"}
-
     try:
         res = requests.post(
             url=url,
             headers=headers,
             auth=HTTPBasicAuth(CLIENT_ID, SECRET),
             data=payload,
-            verify=False,  # Убедитесь, что использование verify=False безопасно для вашей среды
+            verify=False,
         )
         res.raise_for_status()  # проверка на наличие ошибок
         access_token = res.json().get("access_token")
@@ -36,8 +30,6 @@ def get_access_token() -> Any | None:
     except requests.RequestException as e:
         print("Ошибка при получении access token:", e)
         return None
-
-
 def send_prompt(msg: str, access_token: str):
     url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
     payload = json.dumps({
@@ -54,33 +46,26 @@ def send_prompt(msg: str, access_token: str):
         'Accept': 'application/json',
         'Authorization': f'Bearer {access_token}'
     }
-
     try:
         response = requests.post(url, headers=headers, data=payload, verify=False)
         response.raise_for_status()  # проверка на наличие ошибок
         return response.json()["choices"][0]["message"]["content"]
     except requests.RequestException as e:
         print("Ошибка при отправке запроса к GigaChat API:", e)
-        return "Ошибка при получении ответа от GigaChat."
-
-
+        return "Ошибка при получении ответа от GigaChat.",e
 def sent_prompt_and_get_response(msg: str):
     access_token = get_access_token()
-
-    # Создаем сообщение в зависимости от языка
-    messages = f'✨🌈 Придумай необычную, уникальную сказку о {msg} 🧚‍♀️🦄! Пусть это будет история, где царства превращаются в нечто удивительное, а герои сталкиваются с необычными событиями и открытиями 🌟🌌.Больше чудес и смайликов! 😍🎭',
-
-
+    message = (f'✨🌈 Придумай необычную, уникальную сказку о {msg} 🧚‍♀️🦄!'
+               f' Пусть это будет история, где царства '
+               f'превращаются в нечто удивительное, а герои сталкиваются с необычными событиями и '
+               f'открытиями 🌟🌌. '
+               f'Больше чудес и смайликов! 😍🎭')
     if access_token:
-        # Получаем ответ от send_prompt и добавляем смайлики
-        response = send_prompt(messages, access_token)
+        response = send_prompt(message, access_token)
         decorated_response = f'✨🌟 {response} 🌈🧚‍♂️'
         return decorated_response
     else:
         return "Не удалось получить access token."
-
-
-
 def sent_prompt_with_photo_and_get_response(photo, language: str):
     access_token = get_access_token()
     return "🚧 Эта команда пока недоступна. Вернитесь в меню /start 😊"
